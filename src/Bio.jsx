750ee47cdf1ed9669e93c934707ff2c09999dc98
Robert Recalo <rrecalo3@ggc.edu>
2022-11-09 10:47:34 -0500

    Change bio content, add fade-in animations

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -1,29 +1,71 @@
 import React from 'react'
 import {motion, } from 'framer-motion'
+import AnimatedHeader from './AnimatedHeader';
+
+const bioText = [
+  { type: "heading3", text: "I'm Robert" },
+];
+
+const container = {
+  visible: {
+    transition: {
+      staggerChildren: 0.05,
+    }
+  }
+};
 
 const Bio = () => {
   return (
-    <div id="biodiv" className="d-flex flex-column col-12 justify-content-center" style={{marginTop:"8vh"}}>
+    <div id="biodiv" className="d-flex flex-column col-12 justify-content-center" style={{marginTop:"0vh"}}>
     {/** header */}
 
       <div className='d-flex flex-column w-100 align-self-center text-left mt-5 fs-3 px-0 px-md-5 fw-light'>
 
-        <p>
-          I'm Robert - avid software developer and student -<br/><br/>
-          with <b>four years</b> of programming under my belt, I've dipped my feet across the whole spectrum of software
-          <br/>
-          <br/>
-          beginning with Java in a High School AP Comp Sci class, I discovered my passion for<br/> 
-          <span className=''><b>Designing, 
-          Engineering, and Implementing</b></span><br/><i>all</i> kinds of Software.
-          <br/>
-          <br/>
-          since then, I caught interest in <b>Web Development</b> -- gaining experience with the <span className='eht'>MERN stack</span>, both <span className='eht'>SQL and No-SQL databases</span>, 
-          and plenty of practice with <span className='eht'>RESTful Web-API</span>
-        </p>
+        <motion.div
+          initial="hidden"
+          // animate="visible"
+          animate="visible"
+          variants={container}
+          className='d-flex flex-column justify-content-center'>
+
+
+          <div className="container p-0">
+         {bioText.map((item, index)=>{
+          return <AnimatedHeader {...item} key={index} animType={"opacity"} />;
+          })}
+        </div>
+        <motion.p initial={{opacity:0}}animate={{opacity:1, transition:{duration:1, delay:1.5}}}>
+        After discovering my passion for programming, I've been on a long road that has led me here.
+        </motion.p>
+        <motion.p initial={{opacity:0}}animate={{opacity:1, transition:{duration:1, delay:3.5}}}>
+        Beginning with an AP Computer Science class in Highschool, I started with Java.
+        </motion.p>
+        <motion.p initial={{opacity:0}}animate={{opacity:1, transition:{duration:1, delay:5.5}}}>
+        Many languages, years and side-projects later, I found interest in <span className='eht'>Web Development</span>
+        </motion.p>
+        </motion.div>
+        
       </div>
     </div>
   )
 }
 
 export default Bio
+
+
+/*
+
+<p>
+With <b>four years</b> of programming under my belt, I've dipped my feet across the whole spectrum of software
+<br/>
+<br/>
+beginning with Java in a High School AP Comp Sci class, I discovered my passion for<br/> 
+<span className=''><b>Designing, 
+Engineering, and Implementing</b></span><br/><i>all</i> kinds of Software.
+<br/>
+<br/>
+since then, I caught interest in <b>Web Development</b> -- gaining experience with the <span className='eht'>MERN stack</span>, both <span className='eht'>SQL and No-SQL databases</span>, 
+and plenty of practice with <span className='eht'>RESTful Web-API</span>
+</p>
+
+*/
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -11,14 +11,14 @@ const Nav = ({selected, setSelected}) => {
 
   return (
     <div className='d-flex flex-row w-100 justify-content-center gap-5 text-primary'>
-        <motion.button className='bg-transparent border-0' onClick={() => changeSelected("bio")}><motion.h1 
-        animate={{color: selected==="bio" ? "#b6ec8d":  "#b1c5ad",}}>Bio</motion.h1></motion.button>
+        <motion.button className='bg-transparent border-0' onClick={() => changeSelected("bio")}><motion.h2 
+        animate={{color: selected==="bio" ? "#b6ec8d":  "#b1c5ad",}}>Bio</motion.h2></motion.button>
 
 
         <motion.button className='bg-transparent border-0' onClick={() => changeSelected("projects")}>
-            <motion.h1 animate={{color: selected==="projects" ? "#b6ec8d":  "#b1c5ad",}}>Projects</motion.h1></motion.button>
+            <motion.h2 animate={{color: selected==="projects" ? "#b6ec8d":  "#b1c5ad",}}>Projects</motion.h2></motion.button>
         <motion.button className='bg-transparent border-0' onClick={() => changeSelected("experience")}>
-            <motion.h1 animate={{color: selected==="experience" ? "#b6ec8d":  "#b1c5ad",}}>Experience</motion.h1></motion.button>
+            <motion.h2 animate={{color: selected==="experience" ? "#b6ec8d":  "#b1c5ad",}}>Experience</motion.h2></motion.button>
     </div>
   )
 }
